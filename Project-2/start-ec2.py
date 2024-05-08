d3440a684b9fa74268b085a45c5df18cee0d5d08
KiranReddy-Adulla <165106613+highteachingacademy@users.noreply.github.com>
2024-05-08 19:23:45 +0530

    Update start-ec2.py

--- Project-2/start-ec2.py
+++ Project-2/start-ec2.py
@@ -3,11 +3,8 @@ import boto3
 
 #connect to the aws console
 aws_management_console = boto3.session.Session(profile_name="terraform-user")
-ec2_console = aws_management_console.client('ec2')
+ec2_console = aws_management_console.client(service_name='ec2')
 
-response = ec2_console.stop_instances(
-    InstanceId= 'instance-id',
-    Hibernate=True|False,
-    DryRun=True|False
-    Force=True|False
+response = ec2_console.start_instances(
+    InstanceIds= ['instance-id']
 )
